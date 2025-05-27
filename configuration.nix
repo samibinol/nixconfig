2502491c9ff7b8fa70a8748ee1a406c1ee2b0118
sammy <samibinol@icloud.com>
2025-05-27 15:35:17 +0200

    rebuild command issued

--- configuration.nix
+++ configuration.nix
@@ -224,12 +224,12 @@
     sudo.u2fAuth = true;
   };
 
-  services.udev.extraRules = ''
-      ACTION=="remove",\
-       ENV{ID_BUS}=="usb",\
-       ENV{ID_MODEL_ID}=="0407",\
-       ENV{ID_VENDOR_ID}=="1050",\
-       ENV{ID_VENDOR}=="Yubico",\
-       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-  '';  
+  #services.udev.extraRules = ''
+  #    ACTION=="remove",\
+  #     ENV{ID_BUS}=="usb",\
+  #     ENV{ID_MODEL_ID}=="0407",\
+  #     ENV{ID_VENDOR_ID}=="1050",\
+  #     ENV{ID_VENDOR}=="Yubico",\
+  #     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  #'';  
 }
